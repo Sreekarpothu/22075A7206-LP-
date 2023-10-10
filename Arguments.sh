eae175d8aa4bdd81944c022c927d8f2943ad3676
sreekar <144805221+Sreekarpothu@users.noreply.github.com>
2023-10-10 15:26:07 +0530

    Add files via upload

---
+++ Arguments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for arg in "$@"; do
+
+    if [ -e "$arg" ]; then
+        
+        if [ -d "$arg" ]; then
+            echo "$arg is a directory."
+        
+        elif [ -f "$arg" ]; then
+            echo "$arg is a file."
+        else
+            echo "$arg exists but is neither a file nor a directory."
+        fi
+    else
+        echo "$arg does not exist."
+    fi
+done
+
---
+++ list_files_week3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file_list=$(ls)
+
+for file in $file_list
+do
+	echo "$file"
+done
+
+
